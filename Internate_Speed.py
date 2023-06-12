dfe2fe67320090bb7092abf0216542c82ba37dca
Vishal Kumar <kvishalrj2020@gmail.com>
2023-06-12 07:39:42 +0530

    Add files via upload

---
+++ Internate_Speed.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import speedtest as speedtest
+
+def speedcheck():
+    sp = speedtest.Speedtest()
+    sp.get_servers()
+    down = str(round(sp.download()/(10**6), 3))+" Mbps"
+    up = str(round(sp.upload()/(10**6), 3))+" Mbps"
+    lab_down.config(text=down)
+    lab_up.config(text=up)
+
+
+sp = Tk()
+sp.title(" Internate Speed Test")
+sp.geometry("500x650")
+sp.config(bg="Blue")
+
+lab = Label(sp, text="Internate Speed Test", font=("Time New Roman", 30, "bold"),bg="Blue",fg="White")
+lab.place(x=55,y=40,height=60,width=380)
+
+lab = Label(sp, text="Download Speed", font=("Time New Roman", 30, "bold"))
+lab.place(x=55,y=130,height=60,width=380)
+
+lab_down = Label(sp, text="00", font=("Time New Roman", 30, "bold"))
+lab_down.place(x=55,y=200,height=60,width=380)
+
+lab = Label(sp, text="Upload Speed", font=("Time New Roman", 30, "bold"))
+lab.place(x=55,y=290,height=60,width=380)
+
+lab_up = Label(sp, text="00", font=("Time New Roman", 30, "bold"))
+lab_up.place(x=55,y=360,height=60,width=380)
+
+button = Button(sp, text="Check Speed", font=("Time New Roman", 30, "bold"),relief=RAISED,bg="Red",cursor="plus",command=speedcheck)
+button.place(x=60,y=480,height=50,width=380)
+
+sp.mainloop()
